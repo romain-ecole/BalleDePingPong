e883a43b926740b838e69327400624b0d4466088
romain-ecole <158570742+romain-ecole@users.noreply.github.com>
2025-02-27 15:18:11 +0100

    V1

---
+++ dossierFenBalle/classeFenBalle.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace leProjetBalle2PingPong.dossierFenBalle
+{
+    partial class codeFenBalle
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            bBalle = new Button();
+            bStart = new Button();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = SystemColors.ActiveCaption;
+            panel1.Controls.Add(bBalle);
+            panel1.Location = new Point(12, 12);
+            panel1.Margin = new Padding(0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(500, 500);
+            panel1.TabIndex = 0;
+            // 
+            // bBalle
+            // 
+            bBalle.BackColor = Color.Tomato;
+            bBalle.ForeColor = Color.Tomato;
+            bBalle.Location = new Point(245, 490);
+            bBalle.Name = "bBalle";
+            bBalle.Size = new Size(10, 10);
+            bBalle.TabIndex = 0;
+            bBalle.Text = "L";
+            bBalle.UseVisualStyleBackColor = false;
+            // 
+            // bStart
+            // 
+            bStart.Location = new Point(12, 532);
+            bStart.Name = "bStart";
+            bStart.Size = new Size(94, 29);
+            bStart.TabIndex = 0;
+            bStart.Text = "Start";
+            bStart.UseVisualStyleBackColor = true;
+            bStart.Click += bStart_Click;
+            // 
+            // codeFenBalle
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(533, 573);
+            Controls.Add(bStart);
+            Controls.Add(panel1);
+            Name = "codeFenBalle";
+            Text = "Balle";
+            panel1.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Button bBalle;
+        private Button bStart;
+    }
+}
---
+++ dossierFenBalle/classeFenBalle.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace leProjetBalle2PingPong.dossierFenBalle
+{
+    public partial class codeFenBalle : Form
+    {
+
+        private static int AvancerH(bool haut, int phaut)
+        {
+            if (haut == true)
+            {
+                return phaut++;
+            }
+            else
+            {
+                return phaut--;
+            }
+        }
+
+        public codeFenBalle()
+        {
+            InitializeComponent();
+        }
+
+        private void bStart_Click(object sender, EventArgs e)
+        {
+
+            var random = new Random();
+            int i = 0;
+            int pgauche = Convert.ToUInt16(random.Next(489));
+            int phaut = 488;
+            bool haut = false;
+            bool droite = Convert.ToBoolean(random.Next(2) == 1); // 0 = false, 1 = true;
+            int limite = 490;
+            bBalle.Location = new Point(pgauche, phaut);
+            while (i <= 10)
+            {
+                if (pgauche > 1 && pgauche < limite - 1 && phaut > 1 && phaut < limite - 1)
+                {
+                    if (haut == true)
+                    {
+                        phaut--;
+                        bBalle.Location = new Point(pgauche, phaut);
+                    }
+                    else
+                    {
+                        phaut++;
+                        bBalle.Location = new Point(pgauche, phaut);
+                    }
+                    if (droite == true)
+                    {
+                        pgauche--;
+                        bBalle.Location = new Point(pgauche, phaut);
+                        System.Threading.Thread.Sleep(1);
+                    }
+                    else
+                    {
+                        pgauche++;
+                        bBalle.Location = new Point(pgauche, phaut);
+                        System.Threading.Thread.Sleep(1);
+                    }
+                }
+                else
+                {
+                    if (i % 2 == 0)
+                    {
+                        if (haut)
+                        {
+                            haut = false;
+                            phaut++;
+                            bBalle.Location = new Point(pgauche, phaut);
+                            i++;
+                        }
+                        else
+                        {
+                            haut = true;
+                            phaut--;
+                            bBalle.Location = new Point(pgauche, phaut);
+                            i++;
+                        }
+                    }
+                    else
+                    {
+                        if (droite)
+                        {
+                            droite = false;
+                            pgauche++;
+                            bBalle.Location = new Point(pgauche, phaut);
+                            i++;
+                        }
+                        else
+                        {
+                            droite = true;
+                            pgauche--;
+                            bBalle.Location = new Point(pgauche, phaut);
+                            i++;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
